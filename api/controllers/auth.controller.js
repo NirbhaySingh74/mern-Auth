57e773365a2be73be65c9c7d77ab045fcccf479f
Nirbhay <nirbhaysingh943012@gmail.com>
2024-04-20 15:04:10 +0530

    create signIn api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import { User } from "../model/user.model.js";
 import bcryptjs from "bcryptjs";
 import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 export const signup = async (req, res, next) => {
   // console.log(req.body);
   if (!req.body) return res.status(404).json({ message: "filled is required" });
@@ -13,6 +14,27 @@ export const signup = async (req, res, next) => {
     res.status(201).json(newUser);
   } catch (error) {
     //custom error -  next(errorHandler(300, "something went wrong"));
+    next(error);
+  }
+};
+
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) return next(errorHandler(404, "User not found"));
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword)
+      return next(errorHandler(401, "username and password is wrong"));
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    const { password: hashedPassword, ...rest } = validUser._doc;
+    const expiryDate = new Date(Date.now() + 3600000); //1 hour
+
+    res
+      .cookie("access_token", token, { httpOnly: true, expires: expiryDate })
+      .status(200)
+      .json(rest);
+  } catch (error) {
     next(error);
   }
 };
